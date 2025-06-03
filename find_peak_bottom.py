ac932ebb40ab3ef1d62fb308878ed435b9e69de1
fiflow-invest <fiflow.invest@gmail.com>
2025-06-03 11:18:15 +0700

    Add files via upload

--- find_peak_bottom.py
+++ find_peak_bottom.py
@@ -1,6 +1,13 @@
 import pandas as pd
 
 def find_peak(data, var='Close', local_top_number=1):
+    """
+    Function này được sử dụng để tìm các đỉnh của chart giá cổ phiếu
+    :param data: pandas DataFrame phải có tối thiểu các cột Date, Open, High, Low, Close, Volume (phải giống chữ viết hoa).
+    :param var: giá đóng cửa được dùng mặc định
+    :param local_top_number: mặc định là 1
+    :return:
+    """
     extreme = pd.DataFrame()
     data_len = len(data)
     rolling_number = local_top_number * 2 + 1
@@ -9,7 +16,7 @@ def find_peak(data, var='Close', local_top_number=1):
     for i in range(data_len + 1 - rolling_number):
         j = i + rolling_number
         temp_data = temp_df[i:j]
-        if temp_data[var].max() == temp_data[var].iloc[local_top_number]:  #
+        if temp_data[var].max() == temp_data[var].iloc[local_top_number]:  # Nếu điểm ở giữa là điểm cao nhất thì lưu lại
             temp_extreme = temp_data.iloc[[local_top_number]].copy()
             extreme = pd.concat([extreme, temp_extreme], axis=0)
 
@@ -17,6 +24,13 @@ def find_peak(data, var='Close', local_top_number=1):
 
 
 def find_bottom(data, var='Close', local_top_number=1):
+    """
+        Function này được sử dụng để tìm các đáy của giá cổ phiếu
+        :param data: pandas DataFrame phải có tối thiểu các cột Date, Open, High, Low, Close, Volume (phải giống chữ viết hoa).
+        :param var: giá đóng cửa được dùng mặc định
+        :param local_top_number: mặc định là 1
+        :return:
+        """
     extreme = pd.DataFrame()
     data_len = len(data)
     rolling_number = local_top_number * 2 + 1
@@ -25,7 +39,7 @@ def find_bottom(data, var='Close', local_top_number=1):
     for i in range(data_len + 1 - rolling_number):
         j = i + rolling_number
         temp_data = temp_df[i:j]
-        if temp_data[var].min() == temp_data[var].iloc[local_top_number]:
+        if temp_data[var].min() == temp_data[var].iloc[local_top_number]: # Nếu điểm ở giữa là điểm thấp nhất thì lưu lại
             temp_extreme = temp_data.iloc[[local_top_number]].copy()
             extreme = pd.concat([extreme, temp_extreme], axis=0)
 
